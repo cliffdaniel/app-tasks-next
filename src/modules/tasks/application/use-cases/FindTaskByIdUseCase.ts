a48e2fc5ba928e915187e4837bfdf80568d5c4bf
Daniel Arenas <jdaniel.arenas@gmail.com>
2025-01-28 08:47:55 -0500

    add missing JS Docs

--- src/modules/tasks/application/use-cases/FindTaskByIdUseCase.ts
+++ src/modules/tasks/application/use-cases/FindTaskByIdUseCase.ts
@@ -1,6 +1,17 @@
 import { TaskRepository } from '../../domain/TaskRepository';
 import { TaskEntity } from '../../domain/TaskEntity';
 
+/**
+ * Use case for finding a task by its ID.
+ * 
+ * @class FindTaskByIdUseCase
+ * @property {TaskRepository} taskRepository - The repository to handle task retrieval.
+ * @method execute - Executes the process of finding a task by its ID.
+ * 
+ * @param {string} id - The ID of the task to find.
+ * @returns {Promise<TaskEntity | null>} The task entity if found, otherwise null.
+ * @throws {Error} If the ID is missing or the task is not found.
+ */
 export class FindTaskByIdUseCase {
     constructor(private readonly taskRepository: TaskRepository) {}
 
--- src/modules/tasks/application/use-cases/UpdateTaskUseCase.ts
+++ src/modules/tasks/application/use-cases/UpdateTaskUseCase.ts
@@ -2,6 +2,22 @@ import { TaskRepository } from '../../domain/TaskRepository';
 import { TaskEntity } from '../../domain/TaskEntity';
 import { TaskStatusValueObject } from '../../domain/TaskStatusValueObject';
 
+/**
+ * Use case for updating an existing task.
+ * 
+ * @class UpdateTaskUseCase
+ * @property {TaskRepository} taskRepository - The repository to handle task storage and updates.
+ * @method execute - Executes the process of updating a task.
+ * 
+ * @param {Object} taskData - The data to update the task.
+ * @param {string} taskData.id - The ID of the task to update.
+ * @param {string} taskData.title - The updated title of the task.
+ * @param {string} taskData.description - The updated description of the task.
+ * @param {string} taskData.status - The updated status of the task.
+ * @param {string} taskData.userId - The user ID associated with the task.
+ * @returns {Promise<TaskEntity>} The updated task entity.
+ * @throws {Error} If the task is not found.
+ */
 export class UpdateTaskUseCase {
     constructor(private readonly taskRepository: TaskRepository) {}
 
